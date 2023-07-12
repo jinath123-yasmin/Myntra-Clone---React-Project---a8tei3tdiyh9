5ee52e2f8ecb8877bf0db92f9be71d651a5eb810
Jinath Yasmin Laskar <60038325+jinath123-yasmin@users.noreply.github.com>
2023-07-12 13:26:05 +0530

    Update CartContext.js
    added a localstorage

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,18 +3,17 @@ import reducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
 
-// const getLocalCartData = () => {
-//   const localCartData = JSON.parse(localStorage.getItem("thapaCart"));
-//   // if (localCartData === []) {
-//   //   return [];
-//   // } else {
-//     return localCartData;
-//   // }
-// };
-
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("thapaCart");
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+};
 
 const initialState = {
-  cart: [],
+  cart: getLocalCartData(),
   total_item: "",
   total_amount: "",
   shipping_fee: 50000,
@@ -31,11 +30,11 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
 
-  // useEffect(() => {
-  //   // dispatch({ type: "CART_TOTAL_ITEM" });
-  //   // dispatch({ type: "CART_TOTAL_PRICE" });
-  //   localStorage.setItem("thapaCart", JSON.stringify(state.cart));
-  // }, [state.cart]);
+   useEffect(() => {
+    dispatch({ type: "CART_TOTAL_ITEM" });
+    dispatch({ type: "CART_TOTAL_PRICE" });
+    localStorage.setItem("thapaCart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider value={{ ...state, addToCart, removeItem }}>

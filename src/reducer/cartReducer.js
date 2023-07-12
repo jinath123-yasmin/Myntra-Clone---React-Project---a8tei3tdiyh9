2897c8da9d3d9b3e4d7d9027e7eda08b4514ad95
Jinath Yasmin Laskar <60038325+jinath123-yasmin@users.noreply.github.com>
2023-07-12 14:00:46 +0530

    Update cartReducer.js

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -32,36 +32,36 @@ const cartReducer = (state, action) => {
         cart: updatedCart,
       };
     }
-   if (action.type === "CART_TOTAL_ITEM") {
-    let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
-      let { amount } = curElem;
+  //  if (action.type === "CART_TOTAL_ITEM") {
+  //   let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+  //     let { amount } = curElem;
 
-      initialVal = initialVal + amount;
-      return initialVal;
-    }, 0);
+  //     initialVal = initialVal + amount;
+  //     return initialVal;
+  //   }, 0);
 
-    return {
-      ...state,
-      total_item: updatedItemVal,
-    };
-  }
+  //   return {
+  //     ...state,
+  //     total_item: updatedItemVal,
+  //   };
+  // }
 
-  if (action.type === "CART_TOTAL_PRICE") {
-    let total_price = state.cart.reduce((initialVal, curElem) => {
-      let { price, amount } = curElem;
+  // if (action.type === "CART_TOTAL_PRICE") {
+  //   let total_price = state.cart.reduce((initialVal, curElem) => {
+  //     let { price, amount } = curElem;
 
-      initialVal = initialVal + price * amount;
-      // 25000 + 0 = 25000
-      // 10199 + 25000 = 121
+  //     initialVal = initialVal + price * amount;
+  //     // 25000 + 0 = 25000
+  //     // 10199 + 25000 = 121
 
-      return initialVal;
-    }, 0);
+  //     return initialVal;
+  //   }, 0);
 
-    return {
-      ...state,
-      total_price,
-    };
-  }
+  //   return {
+  //     ...state,
+  //     total_price,
+  //   };
+  // }
     return state;
   };
   

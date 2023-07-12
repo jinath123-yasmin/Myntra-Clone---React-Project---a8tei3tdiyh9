749b4c3df9e5a83eeb1592ab9c8f5e6329d2b457
Jinath Yasmin Laskar <60038325+jinath123-yasmin@users.noreply.github.com>
2023-07-12 13:51:08 +0530

    Update cartReducer.js

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -32,7 +32,36 @@ const cartReducer = (state, action) => {
         cart: updatedCart,
       };
     }
-  
+   if (action.type === "CART_TOTAL_ITEM") {
+    let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+      let { amount } = curElem;
+
+      initialVal = initialVal + amount;
+      return initialVal;
+    }, 0);
+
+    return {
+      ...state,
+      total_item: updatedItemVal,
+    };
+  }
+
+  if (action.type === "CART_TOTAL_PRICE") {
+    let total_price = state.cart.reduce((initialVal, curElem) => {
+      let { price, amount } = curElem;
+
+      initialVal = initialVal + price * amount;
+      // 25000 + 0 = 25000
+      // 10199 + 25000 = 121
+
+      return initialVal;
+    }, 0);
+
+    return {
+      ...state,
+      total_price,
+    };
+  }
     return state;
   };
   
